9bb788861aef8c76969d146a1bec57d5911b7252
kwitful <119952091+kwitful@users.noreply.github.com>
2026-07-15 12:08:05 +0300

    tool-writing

--- sys-des-in/tools/file_tools.py
+++ sys-des-in/tools/file_tools.py
@@ -1,12 +1,12 @@
 import os
+from typing import List, Dict, Any
 
-def read_file(filepath:str)->str:
-    """Reads and returns the content of a file."""
-    if not os.path.exists(filepath):
-        raise FileNotFoundError("The file {filepath}does not exist")
+def read_file(filepath:str, start_line: int = 1, limit: int = 5000)->Dict[str, Any]:
+    """Reads the content of a file. Limit exists to prevent token loss.
+
+    Args:
+        filepath: Path to the file.
+        start_limit: 1-indexed line to start from.
+        limit: maximum lines to return
     
-    with open(filepath,'r',encoding='utf-8') as file:
-        return file.read()
-
-
-
+    """
